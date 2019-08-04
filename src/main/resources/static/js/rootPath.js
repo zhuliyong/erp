bfbe7ff2e3c8ea4b8078d804d0c79f390213287d
LENOVO <2625858661@qq.com>
2019-08-04 23:09:04 +0800

    实现单位动态加载

---
+++ src/main/resources/static/js/rootPath.js
@@ -0,0 +1,14 @@
+//js获取项目根路径，如： http://localhost:8083/uimcardprj
+
+var path = getRootPath();
+
+function getRootPath(){
+    //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
+    var curWwwPath=window.document.location.href;
+    //获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
+    var pathName=window.document.location.pathname;
+    var pos=curWwwPath.indexOf(pathName);
+    //获取主机地址，如： http://localhost:8083
+    var localhostPaht=curWwwPath.substring(0,pos);
+    return(localhostPaht);
+}
--- src/main/resources/static/js/unit.js
+++ src/main/resources/static/js/unit.js
@@ -12,7 +12,7 @@ function addUnit (){
     //数据合格则发送请求
     if(flag){
         $.ajax({
-            url: "http://192.168.2.26:8080/unit/new",
+            url: path+"/unit/new",
             data: {
                 "unitCode": code,
                 "unitName": name
@@ -41,18 +41,23 @@ function addUnit (){
 function showPop(){
     $("#newUnit").show();
     $("#backfloat").show();
+    $("#newUnit-code").val("");
+    $("#newUnit-name").val("");
+
 }
 
 /*关闭添加弹窗*/
 function closePop(){
     $("#newUnit").hide();
     $("#backfloat").hide();
+    $("#newUnit-code").val("");
+    $("#newUnit-name").val("");
 }
 
-/*分页列表数据*/
+/*分页数据初始化*/
 function pageList(pageNo,pageSize){
     $.ajax({
-        url: "http://192.168.2.26:8080/unit/page",
+        url: path+"/unit/page",
         data: {
             "pageNo":pageNo,
             "pageSize":pageSize
@@ -61,12 +66,60 @@ function pageList(pageNo,pageSize){
         dataType: "json",
         success: function (res) {
             if(res.status == 1){
-                var pageNo = res.data.pageNum;
-                console.log("pageNo:"+pageNo);
-                var len = res.data.total;
-                var pageSize =res.data.pages;
-                console.log("len:"+len+",pageSize:"+pageSize);
-                page(pageNo,len);
+                //存入隐藏域，初始化加载
+                var pageNo = res.data.pageNum; //当前页
+                var total = res.data.total;  //总数量
+                page(pageNo,total);
+
+                //获取数据并向表体添加
+                var datalength = res.data.list.length;
+                var unitTable=$("#unit-table");
+                for(var i=0;i<datalength;i++){
+                    var unitName = res.data.list[i].unitName;
+                    var unitCode = res.data.list[i].unitCode;
+                    var unitId = res.data.list[i].unitId;
+                    //向表体添加行
+                    unitTable.append( "<tr><td class='unit-id' style='display: none'>" +unitId
+                         + "</td><td>"+unitCode+"</td><td>"+unitName+"</td>"
+                         +'<td><input class="tb-b-edit" type="button" value="编辑">&nbsp;&nbsp;' +
+                          '<input class="tb-b-del" type="button" value="删除"></td>');
+                }
+
+            }
+        },
+        error: function () {
+            console.log("添加单位发生异常")
+        }
+    })
+}
+
+//分页单击事件
+function findPage(pageNo,pageSize){
+    $.ajax({
+        url: path+"/unit/page",
+        data: {
+            "pageNo":pageNo,
+            "pageSize":pageSize
+        },
+        type: "post",
+        dataType: "json",
+        success: function (res) {
+            if(res.status == 1){
+                //获取数据并向表体添加
+                var datalength = res.data.list.length;
+                var unitTable = $("#unit-table");
+
+                for(var i=0;i<datalength;i++){
+                    var unitName = res.data.list[i].unitName;
+                    var unitCode = res.data.list[i].unitCode;
+                    var unitId = res.data.list[i].unitId;
+                    //向表体添加行
+                    unitTable.append( "<tr><td class='unit-id' style='display: none'>" +unitId
+                        + "</td><td>"+unitCode+"</td><td>"+unitName+"</td>"
+                        +'<td><input class="tb-b-edit" type="button" value="编辑">&nbsp;&nbsp;' +
+                        '<input class="tb-b-del" type="button" value="删除"></td>');
+                }
+
             }
         },
         error: function () {
@@ -77,11 +130,13 @@ function pageList(pageNo,pageSize){
 
 
 /*加载分页*/
-function page(pageNo,num){
+//pageNo 当前页码
+//num 数据总条数
+function page(pageNo,total){
     $("#myPage").sPage({
         page:pageNo,//当前页码，必填
-        total:num,//数据总条数，必填
-        pageSize:2,//每页显示多少条数据，默认10条
+        total:total,//数据总条数，必填
+        pageSize:10,//每页显示多少条数据，默认10条
         totalTxt:"共{total}条",//数据总条数文字描述，{total}为占位符，默认"共{total}条"
         showTotal:true,//是否显示总条数，默认关闭：false
         showSkip:true,//是否显示跳页，默认关闭：false
@@ -90,7 +145,7 @@ function page(pageNo,num){
         nextPage:"下一页",//下翻页文字描述，默认“下一页”
         backFun:function(page){
             //点击分页按钮回调函数，返回当前页码
-            console.log(page);
+             findPage(page,10)
         }
     });
 }
